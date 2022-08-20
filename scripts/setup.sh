65512b347d77edd404928b3767a73083ea667c9d
IrvanSN <30742772+IrvanSN@users.noreply.github.com>
2022-08-20 20:18:30 +0700

    docker secret try

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,10 +5,12 @@
 #MONGODB3=`ping -c 1 mongo-repl-3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
 MONGODB1=mongo-repl-1
-MONGODB2=mongo-repl-2
-MONGODB3=mongo-repl-3
 
-echo "**********************************************" ${MONGODB1}
+VAR_HOST_URL=$(cat run/secrets/host-url)
+VAR_DB_NAME=$(cat run/secrets/mongo-name)
+VAR_DB_USERNAME=$(cat run/secrets/mongo-username)
+VAR_DB_PASSWORD=$(cat run/secrets/mongo-password)
+
 echo "Waiting for startup.."
 until curl http://${MONGODB1}:27017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
@@ -18,8 +20,6 @@ done
 # echo curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
 # echo "Started.."
 
-
-echo SETUP.sh time now: `date +"%T" `
 mongo --host ${MONGODB1}:27017 <<EOF
 var cfg = {
     "_id": "rs0",
@@ -28,17 +28,17 @@ var cfg = {
     "members": [
         {
             "_id": 0,
-            "host": "$HOST_URL:27017",
+            "host": \"${VAR_HOST_URL}:27017\",
             "priority": 2
         },
         {
             "_id": 1,
-            "host": "$HOST_URL:27018",
+            "host": \"${VAR_HOST_URL}:27018\",
             "priority": 0
         },
         {
             "_id": 2,
-            "host": "$HOST_URL:27019",
+            "host": \"${VAR_HOST_URL}:27019\",
             "priority": 0
         }
     ],settings: {chainingAllowed: true}
@@ -49,6 +49,6 @@ EOF
 sleep 20
 
 mongo --host ${MONGODB1}:27017 <<EOF
-use $DB_NAME
-db.createUser({user: '$DB_USERNAME', pwd: '$DB_PASSWORD', roles:[{ role: 'readWrite', db: '$DB_NAME' }]});
+use $VAR_DB_NAME
+db.createUser({user: \"${VAR_DB_USERNAME}\", pwd: \"${VAR_DB_PASSWORD}\", roles:[{ role: "readWrite", db: \"${VAR_DB_NAME}\" }]});
 EOF
